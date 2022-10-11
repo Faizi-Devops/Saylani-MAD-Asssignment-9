8ae542063492197d1d9c262e7d56bf4c95adf7de
Faizan Ali <devfaizi435@gmail.com>
2022-10-11 08:59:24 -0700

    Write a TypeScript program to list the properties of a TypeScript object

---
+++ 1) Write a TypeScript program to list the properties of a TypeScript object/script.js
@@ -0,0 +1,6 @@
+var username = {
+    name: "David Rayy",
+    "class": "VI",
+    rollno: 12
+};
+console.log("".concat(username.name, ",").concat(username["class"], ",").concat(username.rollno));
---
+++ 1) Write a TypeScript program to list the properties of a TypeScript object/script.ts
@@ -0,0 +1,13 @@
+interface user {
+    name:string,
+    class:string,
+    rollno:number,
+
+}
+let username:user={
+    name:"David Rayy",
+    class:"VI",
+    rollno:12
+
+}
+console.log(`${username.name},${username.class},${username.rollno}`)
